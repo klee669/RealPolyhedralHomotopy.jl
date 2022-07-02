6522e598ce61f312f485bcb68230451e5ceee478
klee669 <klee669@gatech.edu>
2022-07-01 23:48:07 -0700

    DynamicPolynomials deleted

--- src/RealPolyhedralHomotopy.jl
+++ src/RealPolyhedralHomotopy.jl
@@ -3,14 +3,13 @@ module RealPolyhedralHomotopy
   # internal
   using MixedSubdivisions
   using HomotopyContinuation
-  using DynamicPolynomials
   using LinearAlgebra
   using AbstractAlgebra
   using PolynomialRoots
 
   # exported
   using Reexport
-  @reexport using MixedSubdivisions, HomotopyContinuation, DynamicPolynomials, LinearAlgebra, AbstractAlgebra, PolynomialRoots
+  @reexport using MixedSubdivisions, HomotopyContinuation, LinearAlgebra, AbstractAlgebra, PolynomialRoots
 
   include("certify_patchwork.jl")
   include("generate_binomials.jl")
