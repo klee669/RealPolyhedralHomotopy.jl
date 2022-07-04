08635c9d342045faf9659cc305f376d20546ac65
klee669 <klee669@gatech.edu>
2022-07-04 15:12:37 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,5 +14,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/klee669/RealPolyhedralHomotopy.jl.git"
+    repo = "github.com/klee669/RealPolyhedralHomotopy.jl.git",
+    devbranch = "main"
 )
