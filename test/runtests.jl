e4be200a49982b60e4dcd17a99a5933dea480d44
klee669 <klee669@gatech.edu>
2022-07-24 22:12:22 -0700

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using Test, RealPolyhedralHomotopy
+
+@var x y;
+F = System([-1 - 24000*y + x^3, -9 + 50*x*y - 1*y^2]);
+result = certify_patchwork(F);
+
+@test result == 1
+
+result = certify_patchwork(F; Number_Real_Solutions = true);
+
+@test result[2] == 4
+
+B = generate_binomials(F);
+@test length(B) == 2
+
+realSols = rph_track(B,F)
+@test length(realSols) == 4
+
+println("tests completed successfully")
